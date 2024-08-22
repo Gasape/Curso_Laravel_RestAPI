2eae35dbc4eba6a7802d4613f2a41f81f5838e62
Gasape <gabrielsantoyope@gmail.com>
2024-08-21 17:10:04 -0700

    Sección 18 Implementando las Operaciones para Category

--- app/Http/Controllers/Category/CategoryController.php
+++ app/Http/Controllers/Category/CategoryController.php
@@ -2,64 +2,69 @@
 
 namespace App\Http\Controllers\Category;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
-class CategoryController extends Controller
+class CategoryController extends ApiController
 {
-    /**
-     * Display a listing of the resource.
-     */
+   
     public function index()
     {
-        //
+        //Todas las categorias
+        $categories = Category::all();
+
+        // Respondiendo a la petición
+        return $this->showAll($categories);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        // Reglas de validación
+        $rules = [
+            'name' => 'required',
+            'description' => 'required',
+        ];
+
+        // Validando las reglas
+        $this->validate($request, $rules);
+
+        // Creando la categoria
+        $category = Category::create($request->all());
+
+        // Respondiendo a la petición  
+        return $this->showOne($category,201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    
+    public function show(Category $category)
     {
-        //
+        // Respondiendo con la categoría especifica solicitada
+        return $this->showOne($category,201);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    
+    public function update(Request $request, Category $category)
     {
         //
+        $category->fill($request->only([
+            'name',
+            'description',
+        ]));
+
+        if($category->isClean()){
+            return $this->errorResponse('Debe especificar al menos un valor diferente para actualizar',422);
+        }
+        
+        $category->save();
+        
+        return $this->showOne($category);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    
+    public function destroy(Category $category)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        $category->delete();
+        return $this->showOne($category);
     }
 }
