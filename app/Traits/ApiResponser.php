67f2437e7e14c5c457ec346a789bd552f0f38360
Gasape <gabrielsantoyope@gmail.com>
2024-08-26 10:19:26 -0700

    Sección 30 y 31: Ordenado y filtrado de resultados usando parametros de URL y paginación de resultados

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -4,6 +4,8 @@ namespace App\Traits;
 
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Validator;
 
 trait ApiResponser
 {
@@ -29,7 +31,12 @@ trait ApiResponser
         }
 
         $transformer = $collection->first()->transformCollection;
+        
+        $collection = $this->filterData($collection, $transformer);
 
+        $collection = $this->sortData($collection, $transformer);
+
+        $collection = $this->paginate($collection);
         // $collection = $this->transformData($collection, $transformer);
         // return $this->successResponse(['data' => $collection], $code);
         return new $transformer($collection); //new UserCollection($users);
@@ -40,5 +47,51 @@ trait ApiResponser
         $transformer = $model->transformResource;
         // return $this->successResponse(['data' => $model], $code);
         return new $transformer($model); // new UserResource($users);
+    }
+
+    protected function paginate(Collection $collection)
+    {
+        $rules = [
+            'per_page' => 'integer|min:2|max:50'
+        ];
+
+        Validator::validate(request()->all(), $rules);
+
+        $page = LengthAwarePaginator::resolveCurrentPage();
+
+        $perPage = 15;
+        if(request()->has('per_page')){
+            $perPage = (int)request()->per_page;
+        }
+
+        $results = $collection->slice(($page -1) * $perPage, $perPage)->values();
+
+        $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, ['path' => LengthAwarePaginator::resolveCurrentPath()]);
+
+        $paginated->appends(request()->all());
+
+        return $paginated;
+    }
+
+    protected function filterData(Collection $collection, $transformer)
+    {
+        foreach (request()->query() as $query => $value){
+            $attribute = $transformer::originalAttribute($query);
+
+            if (isset($attribute, $value) ){
+                $collection = $collection->where($attribute, $value);
+            }
+        }
+        return $collection;
+    }
+
+    protected function sortData(Collection $collection, $transformer)
+    {
+        if (request()->has('sort_by')){
+            $attribute = $transformer::originalAttribute(request()->sort_by);
+
+            $collection = $collection->sortBy->{$attribute};
+        }
+        return $collection;
     }
 }
